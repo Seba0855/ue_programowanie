da3c028dccba6f1b2da71b9ee5adaad026403629
Seba0855 <matuszczyksebastian9@gmail.com>
2025-01-11 01:39:07 +0100

    opencv text read poc

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import cv2
+import pytesseract
+import matplotlib.pyplot as plt
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Grayscale, Gaussian blur, Otsu's threshold
+image = cv2.imread('mcd.webp')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (3,3), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
+# Morph open to remove noise and invert image
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+invert = 255 - opening
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Perform text extraction
+data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
+print(data)
 
+cv2.imshow('thresh', thresh)
+cv2.imshow('opening', opening)
+cv2.imshow('invert', invert)
+cv2.waitKey()
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
