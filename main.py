0738192b31b4d1f121044288e83124067e5e0dfc
Seba0855 <matuszczyksebastian9@gmail.com>
2025-01-11 11:21:28 +0100

    opencv cleaned up code

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import cv2
 import pytesseract
-import matplotlib.pyplot as plt
-pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-# Grayscale, Gaussian blur, Otsu's threshold
-image = cv2.imread('mcd.webp')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (3,3), 0)
-thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+pytesseract.pytesseract.tesseract_cmd = r"/opt/homebrew/bin/tesseract"
 
-# Morph open to remove noise and invert image
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-invert = 255 - opening
 
-# Perform text extraction
-data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
-print(data)
+def transformToText(path: str, language: str = "eng", show_result_image: bool = False):
+    image = cv2.imread(path)
+    gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray_scale, (3, 3), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-cv2.imshow('thresh', thresh)
-cv2.imshow('opening', opening)
-cv2.imshow('invert', invert)
-cv2.waitKey()
+    if show_result_image:
+        # Show the result
+        cv2.imshow("Transformed text image", thresh)
+        cv2.waitKey()
 
-if __name__ == '__main__':
-    pass
+    # Text extraction
+    return pytesseract.image_to_string(thresh, lang=language, config="--psm 6")
+
+
+if __name__ == "__main__":
+    text_from_image = transformToText("captcha.png", show_result_image=False)
+    print(text_from_image)
